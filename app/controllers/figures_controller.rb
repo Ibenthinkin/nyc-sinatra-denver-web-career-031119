b21ab8bf6f2e1853ce6b2dc65a72bb840f8ae911
ibenthinkin <benjamin.reilly@gmail.com>
2019-04-05 17:03:46 -0600

    middle of shitshow

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,43 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :"figures/index"
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    erb :"figures/new"
+  end
+
+  post '/figures' do
+    binding.pry
+    figure = Figure.create(name: params[:figure][:name])
+    titles = Title.select do |title|
+      params[:title].include?(title)
+    end
+
+    titles.each do |title|
+      FigureTitle.create(figure: figure, title: title)
+    end
+    redirect '/figures'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    erb :"figures/edit"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :"figures/show"
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find(params[:id])
+    figure.update(name: params[:name])
+    redirect "/figures/#{figure.id}"
+  end
+
+
 end
